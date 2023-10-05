bef42c938afe4f6efc480c0ab2f05d2203d05ea2
Dylan <146326308+Dyldo-666@users.noreply.github.com>
2023-10-05 10:22:09 +0100

    Update 205-while-loop.cpp

--- Tasks/02 - Iteration/205-while-loop/205-while-loop.cpp
+++ Tasks/02 - Iteration/205-while-loop/205-while-loop.cpp
@@ -19,3 +19,22 @@ int main()
 //Task 4: The loop exits when i = 5 
 //Change i = 6 
 //Task 5: The loop will exit straight away when i = 6 so will loop through once
+
+//Task 6: make the code countdown from 10 to 0 in intervals of 2
+
+int main()
+{
+    int i = 10 ;
+
+    puts("Start while loop");
+
+    while (i >= 0)
+    {
+        printf("i = %d\n", i);
+        i = i - 2;    // This will subtract 2 can be written as i -= 2
+    }
+
+    printf("Exiting with i=%d\n", i);
+}
+
+// Difference between while and do while is do while runs once through whereas while doesnt have to run once
