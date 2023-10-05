a42eab7a8d7b9390d6a5e021063819888ca7dc1e
Dylan <146326308+Dyldo-666@users.noreply.github.com>
2023-10-05 10:38:12 +0100

    Update 207-looping-N-times.cpp

--- Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
+++ Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
@@ -14,17 +14,35 @@ int main()
     }
 
     // TASK A - count from 10 down to 0 in steps of -1
-
+    int i;
+    for (i = 10; i >= 0; i -= 1)
+    {
+         printf("i=%d\n", i);
+    }
     // TASK B - count from 10 down to -10 in steps of -2
-
+    int i;
+    for (i = 10; i >= -10; i -= 2)
+    {
+     printf("i=%d\n", i);
+    }
     // TASK C - count from 10 to 200 in steps of 10
-
+     int i;
+    for (i = 10; i <= 200; i += 10)
+    {
+     printf("i=%d\n", i);
+    }
     // TASK D - count from -10 down to 20 in steps of 5
-
+     int i;
+    for (i = -10; i >= 20; i += 5)
+    {
+     printf("i=%d\n", i);
+    }
     // TASK E - can you predict the largest value of i that is displayed?
     for (i = 0; i < 12; i += 3)
     {
         //Perform code tasks
         printf("i=%d\n", i);
     }
+
+    // the largest value for this for loop will be 9 
 }
