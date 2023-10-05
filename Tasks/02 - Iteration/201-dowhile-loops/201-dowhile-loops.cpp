07bca807ab6769d51d2a50325974fc185ae8477a
Dylan <146326308+Dyldo-666@users.noreply.github.com>
2023-10-05 09:41:35 +0100

    Update 201-dowhile-loops.cpp

--- Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
+++ Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
@@ -13,6 +13,22 @@ int main()
     } while (i < 5);            //Repeat if condition is met
                                 // Do while Loop will Loop through at least once before it leaves the loop.
     printf("Finished: i = %d\n", i);
+
+
+     int i = 6;
+
+     printf("Start of Descending while loop\n");
+
+
+    do
+    {
+         printf("i = %d\n", i);
+         i = i - 1;
+    } while (i >= 0);        // the descending loop added aswell
+
+    printf("Finished Descending while Loop: i = %d\n", i);
+
+    
 }
 
 
